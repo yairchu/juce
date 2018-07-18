e2b995420d4e85bcae36f217f2e83ddec00f93e6
John Joseph <mynameisjohnj@gmail.com>
2018-07-17 20:56:07 -0400

    Declare a new project string value for the ARA VST3 Factory IID. We were initially using the IMainFactoryIID, but if we want multiple JUCE plugins to coexist in a single system this is necessary. The UUID is generated on project creation and cached in the .jucer file for future use (regardless of project type, I figure it's not that big a deal to add one more XML attribute.)

--- extras/Projucer/Source/Project/jucer_Project.h
+++ extras/Projucer/Source/Project/jucer_Project.h
@@ -107,6 +107,7 @@ public:
     String getDefaultAAXIdentifierString()               { return getDefaultBundleIdentifierString(); }
 	String getDefaultARAFactoryIDString()                { return "com.yourcompany." + CodeHelpers::makeValidIdentifier (getProjectNameString(), false, true, false) + ".factory"; }
 	String getDefaultARADocumentArchiveID()              { return "com.yourcompany." + CodeHelpers::makeValidIdentifier (getProjectNameString(), false, true, false) + ".aradocumentarchive." + getVersionString(); }
+	String getDefaultARAFactoryUUIDString ()             { return Uuid().toArrayString(); }
 
     String getCompanyNameString() const                  { return companyNameValue.get(); }
     String getCompanyCopyrightString() const             { return companyCopyrightValue.get(); }
@@ -136,6 +137,7 @@ public:
     String getPluginChannelConfigsString() const      { return pluginChannelConfigsValue.get(); }
     String getAAXIdentifierString() const             { return pluginAAXIdentifierValue.get(); }
 	String getARAFactoryIDString() const              { return pluginARAFactoryIDValue.get(); }
+	String getARAVST3FactoryUUIDString() const        { return pluginARAVST3FactoryUUIDValue.toString(); }
 	String getARADocumentArchiveIDString() const      { return pluginARAArchiveIDValue.get(); }
     String getPluginAUExportPrefixString() const      { return pluginAUExportPrefixValue.get(); }
     String getPluginAUMainTypeString() const          { return pluginAUMainTypeValue.get(); }
@@ -412,6 +414,8 @@ private:
                      pluginCodeValue, pluginChannelConfigsValue, pluginCharacteristicsValue, pluginAUExportPrefixValue, pluginAAXIdentifierValue,
                      pluginAUMainTypeValue, pluginRTASCategoryValue, pluginVSTCategoryValue, pluginVST3CategoryValue, pluginAAXCategoryValue,
 		             pluginARAContentTypeValue, pluginARAFactoryIDValue, pluginARAArchiveIDValue, pluginARATransformFlagsValue;
+	
+	Value pluginARAVST3FactoryUUIDValue;
 
     //==============================================================================
     std::unique_ptr<CompileEngineSettings> compileEngineSettings;
