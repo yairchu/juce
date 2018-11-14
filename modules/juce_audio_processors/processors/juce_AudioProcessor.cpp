187140d8df46de3fdbfb8d9d6b0030cb606aaf0b
Yair Chuchem <yairchu@gmail.com>
2018-11-14 18:23:05 +0200

    Revert "Revert "Instead of adding code to the juce::AudioProcessor class, make a subclass of juce::AudioProcessor called juce::ARAAudioProcessor (and an equivalent subclass of juce::AudioProcessorEditor.) This class must live in juce_audio_plugin_client, which avoid including code from juce_audio_plugin_client in juce_audio_procesors. I've still got to update the ARA plug-in template in the Projucer to automatically subclass  the ARA versions of the AudioProcessor/AudioProcessorEditor classes for new projects.""
    This reverts commit 1bd6a4f1b9adf42971da571ed423bbc325575952.

--- modules/juce_audio_processors/processors/juce_AudioProcessor.cpp
+++ modules/juce_audio_processors/processors/juce_AudioProcessor.cpp
@@ -1638,40 +1638,4 @@ void AudioPlayHead::CurrentPositionInfo::resetToDefault()
     bpm = 120;
 }
 
-//==============================================================================
-#if JucePlugin_Enable_ARA
-
-const ARA::ARAPlugInExtensionInstance* AudioProcessor::createARAPlugInExtension (ARA::ARADocumentControllerRef documentControllerRef, ARA::ARAPlugInInstanceRoleFlags knownRoles, ARA::ARAPlugInInstanceRoleFlags assignedRoles)
-{
-    ARA::PlugIn::DocumentController* documentController = reinterpret_cast<ARA::PlugIn::DocumentController*> (documentControllerRef);
-    ARA_VALIDATE_API_ARGUMENT (documentControllerRef, ARA::PlugIn::DocumentController::isValidDocumentController (documentController));
-
-    // verify this is only called once
-    if (araPlugInExtension != nullptr)
-    {
-        ARA_VALIDATE_API_STATE (false && "binding already established");
-        return nullptr;
-    }
-
-    araPlugInExtension.reset(documentController->createPlugInExtensionWithRoles (knownRoles, assignedRoles));
-    return araPlugInExtension->getInstance ();
-}
-
-ARA::PlugIn::PlaybackRenderer* AudioProcessor::getARAPlaybackRenderer() const noexcept
-{
-    return araPlugInExtension ? araPlugInExtension->getPlaybackRenderer() : nullptr;
-}
-
-ARA::PlugIn::EditorRenderer* AudioProcessor::getARAEditorRenderer() const noexcept
-{
-    return araPlugInExtension ? araPlugInExtension->getEditorRenderer() : nullptr;
-}
-
-ARA::PlugIn::EditorView* AudioProcessor::getARAEditorView() const noexcept
-{
-    return araPlugInExtension ? araPlugInExtension->getEditorView() : nullptr;
-}
-
-#endif
-
 } // namespace juce
