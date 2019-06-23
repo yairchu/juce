d8934ce8c3c1879c7aabb96cd73e8ff4990e4850
Yair Chuchem <yairchu@gmail.com>
2019-06-23 11:58:32 +0300

    Fix horizontal scrollbar not giving ability to scroll left when its right side extends further than content

--- examples/ARA/ARASampleProject/Source/TimelineViewport/TimelineViewport.cpp
+++ examples/ARA/ARASampleProject/Source/TimelineViewport/TimelineViewport.cpp
@@ -356,7 +356,7 @@ void TimelineViewport::invalidateViewport()
         if (componentsRange != curRange)
         {
             componentsRange = curRange;
-            hScrollBar->setCurrentRange (componentsRange);
+            hScrollBar->setCurrentRange (componentsRange.getIntersectionWith (getTimelineRange()));
             if (updateComponentsForRange != nullptr)
             {
                 updateComponentsForRange (componentsRange);
